2850d0474505123bf47389441ab09dc433b6eae9
emirsassan <endwse@gmail.com>
2025-06-17 07:12:26 +0300

    chore: replace react-router with tanstack-router (not tested yet)

--- src/components/Initializer.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouteListener } from '@/hooks/event-handler-hooks.ts';
-import { Outlet } from 'react-router-dom';
-
-export const Initializer = () => {
-  useRouteListener();
-
-  return <Outlet />;
-};
